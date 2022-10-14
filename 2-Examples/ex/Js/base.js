ce930728733bbe8502c60a5e012f315581aec048
manhph <manhph@vnext.vn>
2022-10-14 11:16:04 +0700

    push hd

--- 2-Examples/ex/Js/base.js
+++ 2-Examples/ex/Js/base.js
@@ -10,10 +10,15 @@ function bt1() {
   }
 }
 
+// Fibonacci
+8
+
+// khai n0 = 0 , n1 = 1
+
 function bt2() {
-  let arrayRes = [0, 1];
+  let arrayRes = [0, 1, 1, 2, 3];
   let numberPrint;
-  if (NUMBER > 1) {
+  if (NUMBER > 8) {
     for (let index = 2; index < NUMBER; index++) {
       let newFib = arrayRes[index - 2] + arrayRes[index - 1];
       if (newFib <= NUMBER) arrayRes.push(newFib);
@@ -22,14 +27,17 @@ function bt2() {
   } else {
     numberPrint = NUMBER;
   }
-
   for (let index = 0; index < numberPrint; index++) {
     console.log(arrayRes[index]);
   }
+
 }
 
 function bt3() {
-  for (let index = 1; index <= NUMBER + 1; index++) {
+  // index = 1 
+  // index ++ -> index = 2 , 3,4 
+  // index >= 5: out FOr
+  for (let index = 1; index < NUMBER + 1; index++) {
     let star = "";
     for (let indexStar = 1; indexStar <= index; indexStar++) {
       star += "*";
@@ -39,9 +47,9 @@ function bt3() {
 }
 
 function bt4() {
-  for (let index = 1; index <= NUMBER + 1; index++) {
+  for (let index = 1; index <= NUMBER; index++) {
     let space = "";
-    for (let indexSpace = 0; indexSpace <= NUMBER - index; indexSpace++) {
+    for (let indexSpace = 1; indexSpace <= NUMBER - index; indexSpace++) {
       space += " ";
     }
     let star = "";
@@ -50,4 +58,32 @@ function bt4() {
     }
     console.log(space + star);
   }
+}
+
+// h = 4;  * = 2*index - 1;space = h - index 
+//    *            index 1; * = 1; space = 3            
+//   ***           index 2; * = 3; space = 2
+//  *****          index 3; * = 5; space = 1
+// *******         index 4; * = 7; space = 0
+
+function name(params) {
+   let a = 3
+   let b = 5
+   let c = 2
+   let d = 1
+
+   let min = a // 3
+   if(b < min) min = b // min = 3
+   if(c < min) min = c // min = 2
+   if(d < min) min = d 
+
+   // let n = (n-1)/2  = 4.5 float 
+
+   // n =24 : chuc =s parseInt(n/10)
+   //  dvi = n % 10
+
+   // n = 5 nt -> 1, 5
+   // isNT = true
+   // for (i = 2->4) -> 5 % i === 0 -> isNt = false
+   // log(isNT)
 }
